40483cac676ce1996495b57e8d96eff7baf81bec
Dorian Bucaille <74455265+dorian-bucaille@users.noreply.github.com>
2021-04-02 23:35:45 +0200

    Added about page and home page tuitte layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,26 +1,116 @@
 <template>
   <q-layout view="lHr lpR fFf">
 
-    <q-header elevated class="bg-white text-black">
+    <q-header bordered class="bg-white text-black">
       <q-toolbar>
         <q-btn dense flat round icon="menu" @click="left = !left" />
 
         <q-toolbar-title class="text-weight-bold">
-          Tuitteure
+          <span class="gt-sm">{{$route.name}}</span>
+          <q-icon
+            class="header-icon q-pa-md lt-md"
+            name="fas fa-kiwi-bird"
+            size="sm"
+            color="primary"/>
         </q-toolbar-title>
 
       </q-toolbar>
     </q-header>
 
-    <q-drawer show-if-above v-model="left" side="left" bordered>
+    <q-drawer
+      show-if-above
+      v-model="left"
+      :width="280"
+      side="left"
+      bordered>
       <q-icon
+        class="q-pa-md"
         name="fas fa-kiwi-bird"
         size="lg"
         color="primary"/>
+
+        <q-list>
+          <q-item
+            to="/"
+            clickable
+            v-ripple
+            exact>
+            <q-item-section avatar>
+              <q-icon name="home" size="md" />
+            </q-item-section>
+
+            <q-item-section class="text-h6 text-weight-bold">Home</q-item-section>
+          </q-item>
+
+          <q-item
+            to="/about"
+            clickable
+            v-ripple
+            exact>
+            <q-item-section avatar>
+              <q-icon name="info" size="md" />
+            </q-item-section>
+
+            <q-item-section class="text-h6 text-weight-bold">About</q-item-section>
+          </q-item>
+    </q-list>
+
     </q-drawer>
 
     <q-drawer show-if-above v-model="right" side="right" bordered>
-      <!-- drawer content -->
+
+      <q-input
+        placeholder="Search Tuitteure"
+        rounded
+        outlined
+        dense
+        color="primary"
+        class="q-ma-md">
+        <template v-slot:prepend>
+          <q-icon name="search" />
+        </template>
+      </q-input>
+
+      <q-list  padding separator>
+
+        <q-item class="q-pa-md">
+          <q-item-section>
+            <q-item-label overline class="text-grey">Trending in your city</q-item-label>
+            <q-item-label class="text-weight-bold">Dog saved by his old man</q-item-label>
+            <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+          </q-item-section>
+
+          <q-item-section side top>
+            <q-item-label caption>5 min ago</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item class="q-pa-md">
+          <q-item-section>
+            <q-item-label overline class="text-grey">Trending in your city</q-item-label>
+            <q-item-label class="text-weight-bold">Dog saved by his old man</q-item-label>
+            <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+          </q-item-section>
+
+          <q-item-section side top>
+            <q-item-label caption>5 min ago</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item class="q-pa-md">
+          <q-item-section>
+            <q-item-label overline class="text-grey">Trending in your city</q-item-label>
+            <q-item-label class="text-weight-bold">Dog saved by his old man</q-item-label>
+            <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+          </q-item-section>
+
+          <q-item-section side top>
+            <q-item-label caption>5 min ago</q-item-label>
+          </q-item-section>
+        </q-item>
+
+      </q-list>
+
     </q-drawer>
 
     <q-page-container>
@@ -40,3 +130,11 @@ export default {
   }
 }
 </script>
+
+<style lang="sass">
+  .header-icon
+    position: absolute
+    bottom: -3px
+    left: 50%
+    transform: translateX(-50%)
+</style>
---
+++ src/pages/PageHome.vue
@@ -0,0 +1,53 @@
+<template>
+  <q-page>
+    <div class="q-py-lg q-px-md row items-end q-col-gutter-sm">
+
+      <div class="col">
+        <q-input
+          autogrow
+          class="new-tuitte"
+          bottom-slots
+          v-model="newTuitteContent"
+          placeholder="What's up?"
+          counter
+          maxlength="420">
+          <template v-slot:before>
+            <q-avatar size="xl">
+              <img src="https://avatars.githubusercontent.com/u/74455265?v=4" alt="Your profile picture">
+            </q-avatar>
+          </template>
+        </q-input>
+      </div>
+
+      <div class="col col-shrink">
+        <q-btn
+              :disable="!newTuitteContent"
+              class="q-mb-lg"
+              unelevated
+              rounded
+              no-caps
+              color="primary"
+              label="Tuitte" />
+      </div>
+    </div>
+    <q-separator />
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageHome',
+  data() {
+    return {
+      newTuitteContent: ''
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+.new-tuitte
+  textarea
+    font-size: 20px
+    line-height: 1.4 !important
+</style>
