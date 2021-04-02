e023e7ee3a3d550a6e685014b8863dbb821f17aa
Dorian Bucaille <74455265+dorian-bucaille@users.noreply.github.com>
2021-04-02 23:58:01 +0200

    Tuitte layout set up

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -30,7 +30,71 @@
               label="Tuitte" />
       </div>
     </div>
-    <q-separator />
+
+    <q-separator
+      class="divider"
+      size="10px"
+      color="grey-1" />
+
+    <q-list>
+      <q-item class="q-py-md">
+        <q-item-section avatar top>
+          <q-avatar size="xl">
+              <img src="https://avatars.githubusercontent.com/u/74455265?v=4" alt="Your profile picture">
+            </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label class="text-subtitle1">
+            <strong>Dorian Bucaille</strong>
+            <span class="text-grey-7">
+              @dorian_bucaille
+            </span>
+          </q-item-label>
+
+          <q-item-label class="tuitte-content text-body1">Hello there :) Hope you're doing good! I am currently working on this Twitter clone
+            project by watching a great tutorial from freeCodeCamp.org.
+            It is a really good channel, they have taught me a lot about coding!
+          </q-item-label>
+
+          <div class="tuitte-icons row justify-between q-mt-sm">
+            <q-btn
+              flat
+              size=sm
+              round
+              color="grey"
+              icon="far fa-comment" />
+
+              <q-btn
+              flat
+              size=sm
+              round
+              color="grey"
+              icon="fas fa-retweet" />
+
+              <q-btn
+              flat
+              size=sm
+              round
+              color="grey"
+              icon="far fa-heart" />
+
+              <q-btn
+              flat
+              size=sm
+              round
+              color="grey"
+              icon="fas fa-trash" />
+          </div>
+
+        </q-item-section>
+
+        <q-item-section side top>
+          1 min ago
+        </q-item-section>
+      </q-item>
+    </q-list>
+
   </q-page>
 </template>
 
@@ -50,4 +114,15 @@ export default {
   textarea
     font-size: 20px
     line-height: 1.4 !important
+
+.divider
+  border-top: 1px solid
+  border-bottom: 1px solid
+  border-color: $grey-4
+
+.tuitte-content
+  white-space: pre-line
+
+.tuitte-icons
+  margin-left: -5px
 </style>
